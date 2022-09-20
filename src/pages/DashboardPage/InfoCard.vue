4cb3de5b7317d6c26b2a9fb3c548f09c8a6342ef
mr-new-0509 <mr.new0509@gmail.com>
2022-09-21 02:37:38 +0900

    Cards of info

---
+++ src/pages/DashboardPage/InfoCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="rounded-2xl bg-[#2d2d2d] px-4 py-5 flex items-center"
+  >
+    <!-- Icon -->
+    <div class="rounded-2xl px-2 py-4" :class="[iconBgColorClass]">
+      <Icon :icon="icon" class="text-3xl" :class="[iconColorClass]" />
+    </div>
+
+    <!-- label and value -->
+    <div class="ml-2">
+      <p class="text-sm text-[#9596A3] font-semibold">{{ label }}</p>
+
+      <div v-if="isMoney === true" class="flex items-center">
+        <Icon
+          icon="material-symbols:attach-money"
+          class="text-xl text-white font-bold"
+        />
+        <p class="text-md text-white font-semibold">{{ value }}</p>
+      </div>
+      <p v-else class="text-md text-white font-semibold">{{ value }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+export default {
+  name: "info-card",
+  components: {
+    Icon,
+  },
+  props: {
+    icon: String,
+    iconBgColorClass: String,
+    iconColorClass: String,
+    label: String,
+    value: String,
+    isMoney: Boolean,
+  },
+};
+</script>
--- src/pages/DashboardPage/index.vue
+++ src/pages/DashboardPage/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="grid gap-4">
-    <div class="col-span-4">
+  <div class="grid grid-cols-3 gap-4">
+    <div class="col-span-1">
       <!-- Trade -->
       <div class="flex items-center">
         <!-- logo -->
@@ -12,14 +12,14 @@
         <div class="ml-5">
           <p class="text-white text-2xl font-bold">ETH/Palm</p>
           <p class="text-[#9596A3] text-md font-semibold">
-            Palm Token {{ handleLongText(palmTokenAddress, 6, 6) }}
+            Palm Token {{ handleLongText(palmTokenAddress, 6, 3) }}
           </p>
         </div>
 
         <!-- trade button -->
         <button
           class="
-            ml-8
+            ml-12
             py-2
             px-6
             rounded-2xl
@@ -76,9 +76,24 @@
       </div>
 
       <!-- Cards of meta info -->
-      <div class=""></div>
+      <div class="grid grid-cols-2 gap-4 mt-6">
+        <div
+          class="col-span-1"
+          v-for="tokenInfo in tokenInfos"
+          :key="tokenInfo.label"
+        >
+          <info-card
+            :icon="tokenInfo.icon"
+            :iconBgColorClass="tokenInfo.iconBgColorClass"
+            :iconColorClass="tokenInfo.iconColorClass"
+            :label="tokenInfo.label"
+            :value="tokenInfo.value"
+            :isMoney="tokenInfo.isMoney"
+          />
+        </div>
+      </div>
     </div>
-    <div class="col-span-8"></div>
+    <div class="col-span-2"></div>
   </div>
 </template>
 
@@ -86,14 +101,67 @@
 import { Icon } from "@iconify/vue";
 import { PALM_TOKEN_ADDRESS } from "../../utils/constants";
 import { handleLongText } from "../../utils/functions";
+import { defineAsyncComponent } from "@vue/runtime-core";
 
 export default {
   name: "dashboard-page",
   components: {
     Icon,
+    InfoCard: defineAsyncComponent(() => import("./InfoCard.vue")),
   },
   data: () => ({
     palmTokenAddress: PALM_TOKEN_ADDRESS,
+    tokenInfos: [
+      {
+        icon: "material-symbols:attach-money",
+        iconColorClass: "text-[#4AFFDE]",
+        iconBgColorClass: "bg-[#4AFFDE]/[18%]",
+        label: "Total Liquidity",
+        value: "262000.56",
+        isMoney: true,
+      },
+      {
+        icon: "simple-line-icons:graph",
+        iconColorClass: "text-[#4A6DFF]",
+        iconBgColorClass: "bg-[#4A6DFF]/[18%]",
+        label: "24 Hour Volume",
+        value: "2372139.74",
+        isMoney: true,
+      },
+      {
+        icon: "fluent:lock-closed-24-regular",
+        iconColorClass: "text-[#FFC14A]",
+        iconBgColorClass: "bg-[#FFC14A]/[18%]",
+        label: "Total Staked",
+        value: "153234.80",
+        isMoney: true,
+      },
+      {
+        icon: "bx:group",
+        iconColorClass: "text-[#9A4AFF]",
+        iconBgColorClass: "bg-[#9A4AFF]/[18%]",
+        label: "Holders",
+        value: "1386",
+        isMoney: false,
+      },
+      {
+        icon: "ep:histogram",
+        iconColorClass: "text-[#BE7BBB]",
+        iconBgColorClass: "bg-[#BE7BBB]/[18%]",
+        label: "Market Cap",
+        value: "1153234",
+        isMoney: true,
+      },
+      {
+        icon: "icon-park-outline:shoulder-bag",
+        iconColor: "#41709C",
+        iconColorClass: "text-[#41709C]",
+        iconBgColorClass: "bg-[#41709C]/[18%]",
+        label: "Supply",
+        value: "200000000",
+        isMoney: true,
+      },
+    ],
   }),
   methods: {
     handleLongText,
